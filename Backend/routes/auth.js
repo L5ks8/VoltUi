f4adc2e94d4b08882ac6706acd3dea5bf8c9ce04
L5ks8 <L5ks8@users.noreply.github.com>
2026-08-31 02:48:21 +0200

    Update icons and fix JSON errors

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -33,7 +33,7 @@ router.post('/register', async (req, res) => {
         res.json({ message: 'Successfully registered!' });
     } catch (err) {
         console.error(err.message);
-        res.status(500).send('Server error');
+        res.status(500).json({ error: 'Server error' });
     }
 });
 
@@ -77,7 +77,7 @@ router.post('/login', async (req, res) => {
         );
     } catch (err) {
         console.error(err.message);
-        res.status(500).send('Server error');
+        res.status(500).json({ error: 'Server error' });
     }
 });
 
