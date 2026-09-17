ad2b0dbd80cf5bef3e5501235112ea970b7b7b47
L5ks8 <L5ks8@users.noreply.github.com>
2026-09-17 19:30:40 +0200

    refactor(home): remove reload ui button and adjust action layout

--- Backend/server.js
+++ Backend/server.js
@@ -20,20 +20,20 @@ app.get('/api', (req, res) => {
     res.type('json').send(JSON.stringify({
         status: "online",
         name: "Volt API",
-        version: "1.1.8",
+        version: "1.1.9",
         message: "Volt API is operational"
     }));
 });
 
 app.get('/version', (req, res) => {
     res.type('json').send(JSON.stringify({
-        version: "1.1.8"
+        version: "1.1.9"
     }));
 });
 
 app.get('/api/version', (req, res) => {
     res.type('json').send(JSON.stringify({
-        version: "1.1.8"
+        version: "1.1.9"
     }));
 });
 
